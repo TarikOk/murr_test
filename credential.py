6973d9e986a60ab83abc1d9e9def9e37d551620b
TarikOk <35561861+TarikOk@users.noreply.github.com>
2020-02-24 14:15:52 +0200

    add test register

--- credential.py
+++ credential.py
@@ -1,4 +1,20 @@
 URL = {
-    "login": "http://murrengan.ru/murren/",
+    "register": "https://murrengan.ru/murren/register/",
+    "login": "https://murrengan.ru/murren/token_create/",
+    "reset_pass": "https://murrengan.ru/murren/reset_password/",
+    "logout": "https://murrengan.ru/murren/all/",
+}
 
+HEADER = {
+    "header": {
+        "accept": "application/json",
+        "Content-Type": "application/json-patch"
+        }
+}
+
+# need enter your data
+USER = {
+    "email": "",
+    "username": "",
+    "pass": ""
 }
